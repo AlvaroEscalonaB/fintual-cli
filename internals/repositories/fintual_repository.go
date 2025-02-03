00ccebc39047a6da6fa80d18f4f7707493ad7d58
Alvaro Escalona <ajescalona@uc.cl>
2025-02-03 00:11:03 -0300

    8-token: Add token request for fintual repo

--- internals/repositories/fintual_repository.go
+++ internals/repositories/fintual_repository.go
@@ -1,6 +1,7 @@
 package internals
 
 import (
+	"bytes"
 	"encoding/json"
 	"fintual-cli/internals/models"
 	"fmt"
@@ -16,7 +17,9 @@ const (
 
 // Endpoints
 const (
-	Banks = "banks"
+	Banks       = "banks"
+	AccessToken = "access_tokens"
+	Goals       = "goals"
 )
 
 type FintualAPI struct {
@@ -45,18 +48,59 @@ func (fintualPath FintualAPI) GetBanks() ([]models.Bank, error) {
 
 	banks := bankResponse.Data
 	sort.Slice(banks, func(i, j int) bool {
-		iInt, Ierr := strconv.Atoi(banks[i].Id)
-		jInt, Jerr := strconv.Atoi(banks[j].Id)
-		if Ierr != nil {
-			panic(Ierr)
+		iInt, iErr := strconv.Atoi(banks[i].Id)
+		jInt, jErr := strconv.Atoi(banks[j].Id)
+		if iErr != nil {
+			panic(iErr)
 		}
-		if Jerr != nil {
-			panic(Jerr)
+		if jErr != nil {
+			panic(jErr)
 		}
 		return iInt < jInt
 	})
 
 	return banks, nil
+}
+
+func (fintualAPI FintualAPI) ObtainToken(email string, password string) (string, error) {
+	payload := models.UserPayload{
+		User: models.User{
+			Email:    email,
+			Password: password,
+		},
+	}
+	postBody, _ := json.Marshal(payload)
+	responseBody := bytes.NewBuffer(postBody)
+	url := fmt.Sprintf("%s%s", BaseURL, AccessToken)
+
+	resp, err := http.Post(url, "application/json", responseBody)
+
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("error on response, error %d", resp.StatusCode)
+	}
+
+	if resp.StatusCode < 400 {
+		fmt.Printf("Good response with %d\n", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	var tokenResponse models.TokenResponse
+
+	err = json.Unmarshal(body, &tokenResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenResponse.Data.Attributes.Token, nil
 }
 
 var FintualAPIRepository = FintualAPI{BaseURL: BaseURL}
